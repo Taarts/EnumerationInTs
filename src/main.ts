5a23170f3ba8bc13d1f5de9c970b412d56f93606
Tricia Amheiser <tricia@taarts.net>
2022-09-26 16:01:51 -0400

    building a new array of the same length but transformed Like C# "select"

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+console.clear()
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const colors = ['red', 'green', 'blue']
+const lengths: number[] = []
+
+function logSomeColor(color: string, index: number) {
+  const lengthOfColor = color.length
+
+  lengths.push(lengthOfColor)
+  console.log(`The color at position ${index} is ${color}`)
+}
+colors.forEach(logSomeColor)
+console.log(lengths)
